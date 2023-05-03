3abc72d52cdcc95a53bab4f8a2536ad936c50a36
Thomas Rudge <thomasfr@al.insper.edu.br>
2023-05-03 15:30:06 -0300

    Define Posicoes

--- Ep2/Codigo1.py
+++ Ep2/Codigo1.py
@@ -1,1 +1,21 @@
-print("pi")
+def define_posicoes (linha , coluna , orientacao , tamanho):
+
+    posicao = []
+
+    posicao.append([linha,coluna])
+
+    i = 0
+    if orientacao == "vertical":
+        while i < tamanho-1:
+            linha += 1
+            i += 1
+            posicao.append([linha,coluna])
+
+    elif orientacao == "horizontal":
+        while i < tamanho-1:
+            coluna += 1
+            i+= 1
+            posicao.append([linha,coluna])
+
+    return posicao
+    
