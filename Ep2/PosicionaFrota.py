a5c50c963b9447aa60921627067b866f8714943a
Thomas Rudge <thomasfr@al.insper.edu.br>
2023-05-04 10:25:02 -0300

    Posiciona Frota

---
+++ Ep2/PosicionaFrota.py
@@ -0,0 +1,31 @@
+def posiciona_frota(frota):
+
+    listas = []
+
+    tabuleiro = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+    
+
+    for i in frota.values():
+        for coord in i:
+            listas.append(coord)
+
+    
+
+    for i in listas:
+        for coord2 in i:
+            tabuleiro[coord2[0]][coord2[1]] = 1
+
+    
+
+    
+
+    return tabuleiro
